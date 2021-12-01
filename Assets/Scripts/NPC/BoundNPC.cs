63603243272801ea2b971bfb4cec6626194b7e87
UdithPremarathne <IT18137224@my.sliit.lk>
2021-12-01 23:13:48 +0530

    NPC and Dialogs done

---
+++ Assets/Scripts/NPC/BoundNPC.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundNPC : Interactable
+{
+    private Vector3 directionVector;
+    private Transform myTransform;
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Animator anim;
+    public Collider2D bounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        myTransform = GetComponent<Transform>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        ChangeDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerInRange)
+        {
+            Move();
+        }    
+    }
+
+    private void Move()
+    {
+
+        Vector3 temp = myTransform.position + directionVector * speed * Time.deltaTime;
+        if (bounds.bounds.Contains(temp))
+        {
+            myRigidbody.MovePosition(temp);
+        }
+        else
+        {
+            ChangeDirection();
+        }
+        
+    }
+    void ChangeDirection()
+    {
+        int direction = Random.Range(0, 4);
+        switch (direction)
+        {
+            case 0:
+                // Walk right
+                directionVector = Vector3.right;
+                break;
+            case 1:
+                // Walk Up
+                directionVector = Vector3.up;
+                break;
+            case 2:
+                // Walk Left
+                directionVector = Vector3.left;
+                break;
+            case 3:
+                // Walk down
+                directionVector = Vector3.down;
+                break;
+            default:
+                break;
+        }
+        //Change the animation
+        UpdateAnimation();
+    }
+
+    // read direction of the animation
+    void UpdateAnimation()
+    {
+        anim.SetFloat("MoveX", directionVector.x);
+        anim.SetFloat("MoveY", directionVector.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // Change to another direction
+        Vector3 temp = directionVector;
+        int loops = 0;
+        ChangeDirection();
+        while(temp == directionVector && loops < 100)
+        {
+            loops++;
+            ChangeDirection();
+        }
+    }
+}
---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign : Interactable    
+{
+    public GameObject dialogbox;
+    public Text dialogText;
+    public string dialog;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (dialogbox.activeInHierarchy)
+            {
+                dialogbox.SetActive(false);
+            }
+            else
+            {
+                dialogbox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collsion)
+    {
+        if (collsion.CompareTag("Player") && !collsion.isTrigger)
+        {
+            // context is used for enabling the clue Signal
+            context.Raise();
+            playerInRange = false;
+            dialogbox.SetActive(false);
+        }
+    }
+}
